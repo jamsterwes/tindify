2ad13cb9f301e3275137e70a082833e9cb7b3d04
Wes Taylor <jamsterwes@gmail.com>
2022-05-05 17:56:43 -0500

    added volume slider

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 var my_device_id = null;
 var last_track = {id: null};
 var next_tracks = [];
+var player = null;
 
 var playSong = function(song_id) {
     var xhr = new XMLHttpRequest();
@@ -37,17 +38,49 @@ var getRecommendations = function(cb) {
     xhr.send();
 }
 
+var setVolume = function(pct) {
+    var url = "https://api.spotify.com/v1/me/player/volume?device_id=" + my_device_id + "&volume_percent=" + Math.trunc(pct).toString();
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", url);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("accessToken"));
+    
+    xhr.send();
+}
+
+var paintVolume = function(pct) {
+    var color = "var(--slider-fill)";
+    var bkg = "--volume-slidebkg: linear-gradient(90deg, " + color + " 0%, " + color + " " + pct + "%, black " + (pct + 1) + "%);";
+    var iconID = "0";
+    if (pct >= 66) iconID = "3";
+    else if (pct >= 33) iconID = "2";
+    else if (pct >= 1) iconID = "1";
+    var icon = "--slider-icon: url(/assets/volume/volume_icon_" + iconID + ".png);";
+    document.getElementById("volume").style = bkg + icon;
+}
+
+
+document.getElementById("volume-input").oninput = function() {
+    paintVolume(parseInt(document.getElementById("volume-input").value));
+}
+
+document.getElementById("volume-input").onchange = function() {
+    setVolume(parseInt(document.getElementById("volume-input").value));
+}
+
 window.onSpotifyWebPlaybackSDKReady = () => {
     const token = localStorage.getItem("accessToken");
-    const player = new Spotify.Player({
+    player = new Spotify.Player({
         name: "Tindify Web Player",
         getOAuthToken: cb => { cb(token); },
-        volume: 0.5
+        volume: 0.12
     });
+    paintVolume(12);
 
     // Update player state
     player.addListener('player_state_changed', ({ position, duration, paused, track_window: {current_track} }) => {
-        console.log("Current Track: ", current_track);
         // Remove playing from play button
         document.getElementById("play-button").classList.remove("playing");
         // If playing, add to play button
